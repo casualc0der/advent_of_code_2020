44b3930d7338db3115355bc82cb2da8628085cc9
casualc0der <edwardsansome@gmail.com>
2020-12-01 16:03:27 +0000

    Fix indentation

--- day_1.rb
+++ day_1.rb
@@ -7,11 +7,11 @@ class Report
 
   # very slow, but easy to implement ;)
   def parse(entries_to_sum)
-      @data.permutation(entries_to_sum)
-           .select {|x| x.sum == 2020}
-           .map(&:sort)
-           .uniq
-           .flatten(1)
-           .inject(:*)
+    @data.permutation(entries_to_sum)
+         .select {|x| x.sum == 2020}
+         .map(&:sort)
+         .uniq
+         .flatten(1)
+         .inject(:*)
   end
 end

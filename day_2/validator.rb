c916dfef5f265a489e78d41a305403d99c08bfb4
casualc0der <edwardsansome@gmail.com>
2020-12-02 11:12:36 +0000

    Add comments for future browsing

--- day_2/validator.rb
+++ day_2/validator.rb
@@ -2,9 +2,11 @@
   def password_validator(password_line, flag)
     case flag
     when :part_1
+      # Tally number of letters in the password and ensure it is between min/max
      num =  password_line[:password].split('').tally[password_line[:letter]] || 0
      return num >= password_line[:min] && num <= password_line[:max] ? 1 : 0
     when :part_2
+      # Check postions of the letters as validation
       first_index = password_line[:min]
       second_index = password_line[:max]
       password = password_line[:password].split('').unshift(0)

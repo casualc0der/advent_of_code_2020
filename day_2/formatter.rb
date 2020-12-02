21c50ddf9f359516e5afa19f6f3bba2a2555f94a
casualc0der <edwardsansome@gmail.com>
2020-12-02 10:43:57 +0000

    Update formatting on formatter (lol)

--- day_2/formatter.rb
+++ day_2/formatter.rb
@@ -1,11 +1,9 @@
 module Formatter
     def format_line(line)
-
       data_line = line.split(/ /)
       min_max = data_line[0].split('-')
       letter_to_check = data_line[1].gsub(':', '')
       password_to_check = data_line[2]
-
      {
        min: min_max.first.to_i,
        max: min_max.last.to_i,

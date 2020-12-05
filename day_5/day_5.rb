888bee1c9e3cf4cf62d409ace8468dc8488ba2da
casualc0der <edwardsansome@gmail.com>
2020-12-05 11:37:43 +0000

    refactor day 5

--- day_5/day_5.rb
+++ day_5/day_5.rb
@@ -1,46 +1,37 @@
 class TicketScanner
-  attr_reader :data
+  attr_reader :data, :seat_locations
 
   SEAT_ID_MODIFIER = 8
+  ALL_POSSIBLE_TICKETS = (8..976).to_a
 
   def initialize(file)
-    @data = File.read(file).split("\n")
+    @data = File.read(file).split(/\n/)
+    @seat_locations = @data.map {|ticket| seat_finder(ticket)}
   end
 
   def largest_seat_id
-    @data.map {|ticket| seat_finder(ticket)}.max
+    seat_locations.max
   end
 
   def my_seat
-    seen_tickets = @data.map {|ticket| seat_finder(ticket)}
-    all_possible_tickets = (8..976).to_a
-    (all_possible_tickets - seen_tickets).first
+    (ALL_POSSIBLE_TICKETS - seat_locations).first
   end
 
   def seat_finder(str)
     plane_rows = (0..127).to_a
-    plane_columns = (0..7).to_a
-    rows_on_ticket = str[0...7].split(//)
-    cols_on_ticket = str[7..str.size].split(//)
+    plane_cols= (0..7).to_a
+    rows = str[0...7].split(//)
+    cols = str[7..str.size].split(//)
 
-    while plane_rows.size > 1
-      row = rows_on_ticket.shift
-      plane_rows = seat_splitter(row, plane_rows)
-    end
+    plane_rows = seat_splitter(rows.shift, plane_rows) while plane_rows.size > 1
+    plane_cols = seat_splitter(cols.shift, plane_cols) while plane_cols.size > 1
 
-    while plane_columns.size > 1
-      col = cols_on_ticket.shift
-      plane_columns = seat_splitter(col, plane_columns)
-    end
-
-    return plane_rows.first * SEAT_ID_MODIFIER + plane_columns.first
+    return plane_rows.first * SEAT_ID_MODIFIER + plane_cols.first
   end
 
-  def seat_splitter(char, arr)
-    return arr if arr.size == 1
-    pivot = arr.size / 2
+  def seat_splitter(char, arr, pivot = arr.size/2)
     return arr[0...pivot] if char == 'F' || char == 'L'
-    return arr[pivot..arr.size] if char == 'B' || char == 'R'
+    arr[pivot..arr.size]
   end
 end
 

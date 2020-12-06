a82ea048977c18cf1067774861fa02586e7971d5
casualc0der <edwardsansome@gmail.com>
2020-12-06 10:58:11 +0000

    fix formatting

--- day_6/day_6.rb
+++ day_6/day_6.rb
@@ -1,17 +1,22 @@
 class CustomsForm
   attr_reader :data
+
   def initialize(file)
     @data = File.read(file).chomp.split("\n\n")
   end
+
   def sanitized_data
     data.map {|group| group.tr("\n", '') }
   end
+
   def sanitized_group_data
     data.map {|group| group.split("\n").map{|person| [person]} }
   end
+
   def questions_answered
     sanitized_data.map {|group| group.split('').uniq.size }.sum
   end
+
   def collective_questions_answered
     sanitized_group_data.map {|group| checker(group)}.sum
   end
@@ -19,6 +24,6 @@ class CustomsForm
   def checker(arr)
     return arr.inject(&:&).first.size if arr.size == 1
     arr.map {|group| group.first.split('')}.inject(&:&).size
-    end
   end
+end
 

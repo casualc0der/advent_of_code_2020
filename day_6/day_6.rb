d0244e0f70934d525c044fe7dde59b85d8a5b8be
casualc0der <edwardsansome@gmail.com>
2020-12-06 21:18:11 +0000

    rubocop day 6

--- day_6/day_6.rb
+++ day_6/day_6.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class CustomsForm
   attr_reader :data
 
@@ -6,24 +7,24 @@ class CustomsForm
   end
 
   def sanitized_data
-    data.map {|group| group.tr("\n", '') }
+    data.map { |group| group.tr("\n", '') }
   end
 
   def sanitized_group_data
-    data.map {|group| group.split("\n").map{|person| [person]} }
+    data.map { |group| group.split("\n").map { |person| [person] } }
   end
 
   def questions_answered
-    sanitized_data.map {|group| group.split('').uniq.size }.sum
+    sanitized_data.map { |group| group.split('').uniq.size }.sum
   end
 
   def collective_questions_answered
-    sanitized_group_data.map {|group| checker(group)}.sum
+    sanitized_group_data.map { |group| checker(group) }.sum
   end
 
   def checker(arr)
     return arr.inject(&:&).first.size if arr.size == 1
-    arr.map {|group| group.first.split('')}.inject(&:&).size
+
+    arr.map { |group| group.first.split('') }.inject(&:&).size
   end
 end
-
